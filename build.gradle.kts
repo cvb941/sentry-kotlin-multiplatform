486f1098eca47002eb6839f2a08c4f037134146b
Lukas Kusik <lukas.kusik@gmail.com>
2023-02-01 17:54:17 +0100

    Update KGP to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("com.android.tools.build:gradle:7.2.2")
     }
 }
